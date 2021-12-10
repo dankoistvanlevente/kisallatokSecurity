d7def8035b23bd5e4141142dcdf41186095c498f
20g_dankoi <dankopisti16@gmail.com>
2021-12-10 08:12:17 +0100

    commit

---
+++ src/main/java/hu/kisallatokSecurity/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package hu.kisallatokSecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth){
+        try {
+            auth.inMemoryAuthentication()
+                    .withUser("user").password("{noop}user").roles("USER")
+                    .and()
+                    .withUser("admin").password("{noop}admin").roles("ADMIN", "USER");
+        } catch (Exception ex) {
+            System.out.println("ex in SecurityConfig.configureAuth(): "+ex.getMessage());
+        }
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+}
